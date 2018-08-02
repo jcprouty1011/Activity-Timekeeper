b23af121dd1c3efcc6831fb3da95e3be1650579e
Jeffrey Prouty <jcprouty@gmail.com>
2018-08-02 15:15:37 -0400

    Added basic activity detail view

---
+++ Activity Timekeeper/ActivityDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ActivityDetailViewController.swift
+//  Activity Timekeeper
+//
+//  Created by Jeffrey Prouty on 8/1/18.
+//  Copyright © 2018 Jeffrey Prouty. All rights reserved.
+//
+
+import UIKit
+
+class ActivityDetailViewController: UIViewController {
+    
+    var activity: Activity!
+    @IBOutlet weak var activityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityLabel.text = activity.name
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+ 
+
+}
--- Activity Timekeeper/ActivityTableViewController.swift
+++ Activity Timekeeper/ActivityTableViewController.swift
@@ -56,17 +56,18 @@ class ActivityTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            activities.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -83,15 +84,19 @@ class ActivityTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "ActivityDetailSegue" {
+            let activityDetailViewController = segue.destination as! ActivityDetailViewController
+            activityDetailViewController.activity = activities[tableView.indexPathForSelectedRow!.row]
+        }
     }
-    */
+    
     
     @IBAction func unwindToActivityTable (segue: UIStoryboardSegue) {
         tableView.reloadData()
